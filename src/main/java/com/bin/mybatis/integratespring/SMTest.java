9fc7a7c138f6ff92c3d7389afc200b3e054a2c7b
xiaobin <xbliu564@163.com>
2017-12-23 21:29:34 +0800

    Spring编程式事务

--- src/main/java/com/bin/mybatis/integratespring/SMTest.java
+++ src/main/java/com/bin/mybatis/integratespring/SMTest.java
@@ -1,15 +1,15 @@
 package com.bin.mybatis.integratespring;
 
-import java.util.Date;
-
+import com.bin.mybatis.integratespring.bean.User;
+import com.bin.mybatis.integratespring.mapper.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.support.TransactionTemplate;
 
-import com.bin.mybatis.integratespring.bean.User;
-import com.bin.mybatis.integratespring.mapper.UserMapper;
+import java.util.Date;
 
 @RunWith(SpringJUnit4ClassRunner.class)// 使用Spring的测试框架.
 @ContextConfiguration("/com/bin/mybatis/integratespring/beans.xml")// 加载spring的配置文件beans.xml
@@ -17,6 +17,8 @@ public class SMTest {
 	
 	@Autowired
 	private UserMapper userMapper ;
+//	@Autowired
+//	private TransactionTemplate transactionTemplate ;
 	
 	@Test
 	public void testAdd(){
